e9a1d4b7978429c6edb1e3f7029e87bb6c425882
MATHEUS LUCAS DE FREITAS ZACARIAS <25203742@CFP127ALUNO.LOCAL>
2026-02-03 16:24:11 -0300

    Adicionando novas cores em Português

--- javaScript/main.js
+++ javaScript/main.js
@@ -16,25 +16,54 @@
             document.documentElement.style.setProperty("--color-text", "white")
         }else if(corUsuario == "roxo"){
             document.documentElement.style.setProperty("--color-bg", "purple")
+            document.documentElement.style.setProperty("--color-text", "black")
         }else if(corUsuario == "vermelho"){
             document.documentElement.style.setProperty("--color-bg", "red")
+            document.documentElement.style.setProperty("--color-text", "black")
         }else if(corUsuario == "verde"){
             document.documentElement.style.setProperty("--color-bg", "green")
-
+            document.documentElement.style.setProperty("--color-text", "black")
+        }else if(corUsuario == "black"){
+            document.documentElement.style.setProperty("--color-bg", "black")
+            document.documentElement.style.setProperty("--color-text", "white")
+        }else if( corUsuario == "amarelo"){
+            document.documentElement.style.setProperty("--color-bg", "yellow")
+            document.documentElement.style.setProperty("--color-text", "black")
+        }else if(corUsuario == "azul"){
+            document.documentElement.style.setProperty("--color-bg", "blue")
+            document.documentElement.style.setProperty("--color-text", "black")
+        }else if(corUsuario == "rosa"){
+            document.documentElement.style.setProperty("--color-bg", "pink")
+            document.documentElement.style.setProperty("--color-text", "black")
+        }else if (corUsuario == "branco"){
+            document.documentElement.style.setProperty("--color-bg", "white")
+            document.documentElement.style.setProperty("--color-text", "black")
+        }else if(corUsuario == "marrom"){
+            document.documentElement.style.setProperty("--color-bg", "brown")
+            document.documentElement.style.setProperty("--color-text", "black")
+        }else if(corUsuario == "bege"){
+            document.documentElement.style.setProperty("--color-bg", "beige")
+            document.documentElement.style.setProperty("--color-text", "black")
+        }
+        else{
+            document.documentElement.style.setProperty("--color-text", "black")
         }
         
     }
 
     function fundoVerde(){
         document.documentElement.style.setProperty("--color-bg", "beige")
+        document.documentElement.style.setProperty("--color-text", "black")
     }
 
     function fundoAmarelo(){
         document.documentElement.style.setProperty("--color-bg", "yellow")
+        document.documentElement.style.setProperty("--color-text", "black")
     }
 
     function fundoAqua(){
         document.documentElement.style.setProperty("--color-bg", "aqua")
+        document.documentElement.style.setProperty("--color-text", "black")
     }
 
     botaoTrocarCor.addEventListener("click", trocaCor)
