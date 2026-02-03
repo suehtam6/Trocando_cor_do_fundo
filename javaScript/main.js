1953ea21df3213d09b93cbb99fb4f5767e3777d5
unknown <suehtam6>
2026-02-03 19:56:55 -0300

    Novos botões feitos, espero atualizar mais no futuro

--- javaScript/main.js
+++ javaScript/main.js
@@ -5,6 +5,12 @@
     const botaoBeige = document.getElementById("botao-beige")
     const botaoAmarelo = document.getElementById("botao-amarelo")
     const botaoAqua = document.getElementById("botao-aqua")
+    const botaoVermelho = document.getElementById("botao-vermelho")
+    const botaoVerde = document.getElementById("botao-verde")
+    const botaoRoxo = document.getElementById("botao-roxo")
+    const botaoRosa = document.getElementById("botao-rosa")
+    const botaoCinza = document.getElementById("botao-cinza")
+    const botaoAzul = document.getElementById("botao-azul")
     
 
     function trocaCor(){
@@ -33,7 +39,7 @@
             document.documentElement.style.setProperty("--color-text", "black")
         }else if(corUsuario == "azul"){
             document.documentElement.style.setProperty("--color-bg", "blue")
-            document.documentElement.style.setProperty("--color-text", "black")
+            document.documentElement.style.setProperty("--color-text", "white")
         }else if(corUsuario == "rosa"){
             document.documentElement.style.setProperty("--color-bg", "pink")
             document.documentElement.style.setProperty("--color-text", "black")
@@ -46,8 +52,10 @@
         }else if(corUsuario == "bege"){
             document.documentElement.style.setProperty("--color-bg", "beige")
             document.documentElement.style.setProperty("--color-text", "black")
-        }
-        else{
+        }else if(corUsuario == "blue"){
+            document.documentElement.style.setProperty("--color-bg", "blue")
+            document.documentElement.style.setProperty("--color-text", "white")
+        }else{
             document.documentElement.style.setProperty("--color-text", "black")
         }
         
@@ -68,6 +76,36 @@
         document.documentElement.style.setProperty("--color-text", "black")
     }
 
+        function fundoVermelho(){
+        document.documentElement.style.setProperty("--color-bg", "red")
+        document.documentElement.style.setProperty("--color-text", "black")
+    }
+
+        function fundoVerde(){
+        document.documentElement.style.setProperty("--color-bg", "green")
+        document.documentElement.style.setProperty("--color-text", "black")
+    }
+
+        function fundoRoxo(){
+        document.documentElement.style.setProperty("--color-bg", "purple")
+        document.documentElement.style.setProperty("--color-text", "black")
+    }
+
+        function fundoRosa(){
+        document.documentElement.style.setProperty("--color-bg", "pink")
+        document.documentElement.style.setProperty("--color-text", "black")
+    }
+
+        function fundoCinza(){
+        document.documentElement.style.setProperty("--color-bg", "gray")
+        document.documentElement.style.setProperty("--color-text", "black")
+    }
+
+        function fundoAzul(){
+        document.documentElement.style.setProperty("--color-bg", "blue")
+        document.documentElement.style.setProperty("--color-text", "white")
+    }
+
 function fundoAleatorio() {
     // Com esta function eu posso criar a cor aleatoria sendo hexadecimal e linkando no botão de "Cor Aleatoria"
     const corGerada = '#' + Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0');
@@ -79,3 +117,9 @@ function fundoAleatorio() {
     botaoAmarelo.addEventListener("click", fundoAmarelo)
     botaoAqua.addEventListener("click", fundoAqua)
     botaoCorAleatoria.addEventListener("click",fundoAleatorio)
+    botaoVermelho.addEventListener("click", fundoVermelho)
+    botaoVerde.addEventListener("click", fundoVerde)
+    botaoRoxo.addEventListener("click", fundoRoxo)
+    botaoRosa.addEventListener("click", fundoRosa)
+    botaoCinza.addEventListener("click", fundoCinza)
+    botaoAzul.addEventListener("click", fundoAzul)
